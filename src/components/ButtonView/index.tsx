bb9e84adb37dda8edcce9ccd54b62a903ab9401d
farshid karimi <karimi_f@yaftar.ir>
2023-08-12 14:12:47 +0430

    react declare but not use

--- src/components/ButtonView/index.tsx
+++ src/components/ButtonView/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Button from "../Button";
 import styles from "./index.module.scss";
 
--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ButtonView from "../ButtonView";
 import styles from "./index.module.scss";
 
