8dc943368f84d6a346a46ddc02ef4f066e6b69c9
farshid karimi <karimi_f@yaftar.ir>
2023-08-12 13:59:06 +0430

    correct colors

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -7,11 +7,19 @@ const Buttons = () => {
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <h1>Buttons</h1>
-        <ButtonView title="<Button />" />
-        <ButtonView title="<Button variant=”outline” />" variant="outline" />
-        <ButtonView title="<Button variant=”text” />" variant="text" />
-        <ButtonView title="<Button disableShadow />" disableShadow />
-        <div className={styles.inline}>
+        <div className={styles.row}>
+          <ButtonView title="<Button />" />
+        </div>
+        <div className={styles.row}>
+          <ButtonView title="<Button variant=”outline” />" variant="outline" />
+        </div>
+        <div className={styles.row}>
+          <ButtonView title="<Button variant=”text” />" variant="text" />
+        </div>
+        <div className={styles.row}>
+          <ButtonView title="<Button disableShadow />" disableShadow />
+        </div>
+        <div className={styles.row}>
           <ButtonView title="<Button disabled />" disabled text="Disabled" />
           <ButtonView
             title="<Button variant=”text” disabled />"
@@ -20,7 +28,7 @@ const Buttons = () => {
             text="Disabled"
           />
         </div>
-        <div className={styles.inline}>
+        <div className={styles.row}>
           <ButtonView
             title="<Button startIcon=”local_grocery_store” />"
             startIcon="local_grocery_store"
@@ -30,12 +38,12 @@ const Buttons = () => {
             endIcon="local_grocery_store"
           />
         </div>
-        <div className={styles.inline}>
+        <div className={styles.row}>
           <ButtonView title="<Button size=”sm” />" size="sm" />
           <ButtonView title="<Button size=”md” />" size="md" />
           <ButtonView title="<Button size=”lg” />" size="lg" />
         </div>
-        <div className={styles.inline}>
+        <div className={styles.row}>
           <ButtonView title="<Button color=”default” />" color="default" />
           <ButtonView title="<Button color=”primary” />" color="primary" />
           <ButtonView title="<Button color=”secondary” />" color="secondary" />
